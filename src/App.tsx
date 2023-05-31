e0a840823ffc788ff9908407a8d60940c829ab00
Arcturyus <117201023+Arcturyus@users.noreply.github.com>
2023-05-31 19:09:05 +0200

    ro

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,8 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le blind test</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        <p>Il va falloir modifier le code pour fai un vrai blind test !</p>
+        <button onClick={goToNextTrack}>AAAAAAAAAAAAAAA</button>
       </div>
       <div className="App-buttons"></div>
     </div>
